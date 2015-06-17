2e7d287dd61233d4b5ebb60a2a4ee02d9455b9f2
Fahimeh1983 <fahimeh.baftizadeh@gmail.com>
2015-06-17 18:07:02 -0400

    Update longest-alphabet-order.py

--- longest-alphabet-order.py
+++ longest-alphabet-order.py
@@ -1,17 +1,17 @@
 s = 'rstuwyabcdefghkl'
-
-sort = ''
-guessord = 0
-longest = 0
+# s is an example
+sort = '' 
+guessord = 0 # all the alphabetical orders are above 90
+longest = 0  # the longest one has a length of zero
 
 for i in s :
     if ord(i) >= guessord:
-        sort = sort + i
+        sort = sort + i   # if the order of the next char is more than the previous one, it will be added to our string
         if len(sort) > longest :
             longest = len(sort)
             result = str(sort)
     else :
-        sort = i
+        sort = i  # if the order of rht next char is less than the previous one, the string will restart
     guessord = ord(i)
         
 
