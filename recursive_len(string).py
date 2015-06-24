5f3df46f42686c0abde85edd87d6c5c9e49d8028
Fahimeh1983 <fahimeh.baftizadeh@gmail.com>
2015-06-24 17:01:10 -0400

    Create recursive_len(string).py

---
+++ recursive_len(string).py
@@ -0,0 +1,12 @@
+#recursive search for the lenght of a string
+def lenRecur(aStr):
+    '''
+    aStr: a string
+    
+    returns: int, the length of aStr
+    '''
+    # Your code here
+    if aStr == '':
+        return 0
+    else:
+        return 1 + lenRecur(aStr[1:])
