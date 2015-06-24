6eb9f8d05aec3e98b875c3646332532206ade303
Fahimeh1983 <fahimeh.baftizadeh@gmail.com>
2015-06-24 15:46:08 -0400

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,11 @@
+# Fibonacci
+
+def fib(x):
+    if x< 0 or type(x) != int :
+        return "None"
+    elif x == 0 or x == 1 :
+        return 1
+    else:
+        return fib(x-1) + fib (x-2)
+
+print fib(5) 
