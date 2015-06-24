917052dc50acfc911794dacaad2c49dc52f687db
Fahimeh1983 <fahimeh.baftizadeh@gmail.com>
2015-06-24 14:55:02 -0400

    Update recursive_greatest_common_divisors.py

--- recursive_greatest_common_divisors.py
+++ recursive_greatest_common_divisors.py
@@ -1,8 +1,6 @@
-A clever mathematical trick that makes it easy to find greatest common divisors. Suppose that a and b are two positive integers:
-
-If b = 0, then the answer is a
-
-Otherwise, gcd(a, b) is the same as gcd(b, a % b)
+#A clever mathematical trick that makes it easy to find greatest common divisors. Suppose that a and b are two positive integers:
+#If b = 0, then the answer is a
+#Otherwise, gcd(a, b) is the same as gcd(b, a % b)
 
 def gcdRecur(a, b):
     '''
